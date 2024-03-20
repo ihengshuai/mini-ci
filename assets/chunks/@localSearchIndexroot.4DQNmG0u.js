7bb8c532b38424cdc0bc24e83c6c760739416306
ihengshuai <wsm_1105@163.com>
2024-03-20 07:58:04 +0000

    update page

---
+++ assets/chunks/@localSearchIndexroot.4DQNmG0u.js
@@ -0,0 +1,1 @@
+const e='{"documentCount":1,"nextId":1,"documentIds":{"0":"/api/base.html#api-reference"},"fieldIds":{"title":0,"titles":1,"text":2},"fieldLength":{"0":[2,1,1]},"averageFieldLength":[2,1,1],"storedFields":{"0":{"title":"API Reference","titles":[]}},"dirtCount":0,"index":[["reference",{"0":{"0":1}}],["api",{"0":{"0":1}}]],"serializationVersion":2}';export{e as default};
