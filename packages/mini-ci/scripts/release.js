367f3b9fbcaffedd1f73f0c4152f4c453ca0e12f
取舍 <wsm_1105@163.com>
2024-03-20 12:41:05 +0800

    chore: 使用node运行esbuild进行打包(#0)

---
+++ packages/mini-ci/scripts/release.js
@@ -0,0 +1,22 @@
+const { build } = require("esbuild");
+const { copy } = require("esbuild-plugin-copy");
+
+build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outfile: "dist/index.js",
+  format: "cjs",
+  platform: "node",
+  target: "node14",
+  minify: true,
+  packages: "external",
+  plugins: [
+    copy({
+      resolveFrom: "cwd",
+      assets: {
+        from: ["README.md"],
+        to: ["dist/README.md"],
+      },
+    }),
+  ],
+});
